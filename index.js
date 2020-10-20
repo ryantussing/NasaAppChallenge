0cd934b44458e102e9a84b5eedce6664785e35a7
Cdubles102403 <44614396+Cdubles102403@users.noreply.github.com>
2020-10-20 14:24:49 -0400

    Add triangle strip

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.use(express.static('public'))
+app.use(express.static('public')) //sends public folder to frontend
 
 
-app.get('/', (req, res) => {
-  console.log("user connected")
-  res.send("public/index.js")
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- public/client.js
+++ public/client.js
@@ -1,12 +1,33 @@
+var cols, rows;
+var scl = 20;
+var w = 1400;
+var h = 1000;
+
+var flying = 0;
+
+var terrain = [];
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
+frameRate(60)
+  createCanvas(600, 600, WEBGL);
+  cols = w / scl;
+  rows = h / scl;
+
+}
+
+function draw() {
+
+
+  background(0);
+
+  fill(255);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x * scl, y * scl);
+      vertex(x * scl, (y + 1) * scl);
     }
-    ellipse(mouseX, mouseY, 80, 80);
+    endShape();
   }
+}
