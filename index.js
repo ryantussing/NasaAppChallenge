b970e7f70c8def74eb73c7f18acb7fab984cf252
Cdubles102403 <44614396+Cdubles102403@users.noreply.github.com>
2020-10-20 13:56:49 -0400

    connect back and front end

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("it works")
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  console.log("user connected")
+  res.send("public/index.js")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
