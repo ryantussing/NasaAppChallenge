faf49eefa395f3cda29fb12c35a986f5adbbfc61
cdubles <33189090+cdubles@users.noreply.github.com>
2020-10-21 22:39:11 -0400

    add easycam for better movment of camera

--- public/client.js
+++ public/client.js
@@ -1,32 +1,37 @@
 var cols, rows;
 var scl = 20;
-var w = 1400;
-var h = 1000;
+var w = 800;
+var h = 800;
 
-var flying = 0;
+var cam;
 
 var terrain = [];
 
 function setup() {
-frameRate(60)
-  createCanvas(600, 600, WEBGL);
-  cols = w / scl;
-  rows = h / scl;
+createCanvas(w, h, WEBGL);
+createEasyCam();
 
+cols = w / scl;
+rows = h / scl;
+
+for(var i=0;i<=cols;i++){
+  terrain[i]=[]
+  for(var y=0;y<rows;y++){
+      terrain[i][y]=random(1,100)
+    }
+  }
 }
 
 function draw() {
-
-
-  background(0);
+background(0);
 
   fill(255);
   translate(-w / 2, -h / 2);
   for (var y = 0; y < rows - 1; y++) {
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
-      vertex(x * scl, y * scl);
-      vertex(x * scl, (y + 1) * scl);
+      vertex(x * scl, y * scl,terrain[y][x]);
+      vertex(x * scl, (y + 1) * scl,terrain[y+1][x]);
     }
     endShape();
   }
