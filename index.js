a5343a9b8561e16295d3dcf2011a49a1eae5c5fa
cdubles <33189090+cdubles@users.noreply.github.com>
2020-11-11 22:38:21 -0500

    updated .gitignore, added packages to node, added start rutine, switched python to js in index.js

--- index.js
+++ index.js
@@ -1,42 +1,90 @@
-const express = require('express')
-const app = express()
-const parser = require('body-parser')
-const multer = require('multer')
-const {spawn} = require('child_process')
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const multer = require("multer");
+const csv = require("csv-parser");
+const fs = require("fs");
 //setup for multer data
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads')
+    cb(null, "uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-var upload = multer({ storage: storage })
+    cb(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 //port to run on
-const port = 8080
+const port = 8080;
 //send public folder to client
-app.use(express.static('public')) //sends public folder to frontend
+app.use(express.static("public")); //sends public folder to frontend
 app.use(parser.urlencoded({ extended: false }));
-app.use(parser.json())
+app.use(parser.json());
 //start server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 //post request path for uploading file to server
-app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-  const file = req.file
-  var data = spawn('python',['converter.py',file.filename])
-  data.stdout.on('data',function(data){
-    console.log('python output')
-    console.log(data.toString())
-  })
-  if (!file) {
-    res.send("please upload a file")
+app.post("/uploadfile", upload.single("myFile"), (req, res, next) => {
+  
+  if (!req.file) {
+    res.send("please upload a file");
     return false;
   }
-    console.log(file)
-    res.redirect('..')
-    res.end()
-})
+  const file = req.file;
+  //TODO:make this in native JS(replace python)
+  // var data = spawn('python',['converter.py',file.filename])
+  // data.stdout.on('data',function(data){
+  //   console.log('python output')
+  //   console.log(data.toString())
+  // })
+  var x = [];
+  var y = [];
+  var z = [];
+  var smallX = [];
+  var smallY = [];
+  var smallZ = [];
+  var smallSlope = [];
+  var slope = [];
+  fs.createReadStream("uploads/" + file.originalname)
+    .pipe(csv())
+    .on("data", (row) => {
+      length = Object.keys(row).length;
+      var currentColumn = 0;
+      for (i in row) {
+        //console.log(row[i]);
+        if (currentColumn == 0) {
+          x.push(row[i]);
+        } else if (currentColumn == 1) {
+          y.push(row[i]);
+        } else if (currentColumn == 2) {
+          z.push(row[i]);
+        } else if (currentColumn == 3) {
+          slope.push(row[i]);
+        } else {
+          console.log("error in parsing file: Line Count Error");
+        }
+        currentColumn += 1;
+      }
+    })
+    .on("end", () => {
+      console.log("file processing completed");
+      for (i in x) {
+        if (i % 7200 == 0) {
+          smallX.push(x[i]);
+          smallY.push(y[i]);
+          smallZ.push(z[i]);
+          smallSlope.push(slope[i]);
+        }
+      }
+      console.log(smallX);
+      console.log(smallY);
+      console.log(smallZ);
+      console.log(smallSlope);S
+    });
+
+  console.log(file);
+  res.redirect(".."); //redirect back to starting page (stop page rerouting)
+  res.end();
+});
